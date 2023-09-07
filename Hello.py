5b618e7c4b88e74b924e0f22d2e11f3ade9bd7c8
Aleksandr Iakubovich <136030897+aiakubovich@users.noreply.github.com>
2023-09-07 01:42:35 -0500

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -24,29 +24,14 @@ def run():
         page_icon="👋",
     )
 
-    st.write("# Welcome to Streamlit v 0.1! 👋")
+    st.write("# Welcome to Streamlit v 0.3! 👋")
+    
+    st.write(print('print via write', os.environ["OPENAI_API_KEY"]))
+    
 
     st.sidebar.success("Select a demo above.")
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
-
-    print(os.environ.get("OPENAI_API_KEY"))
+    
 
 
 if __name__ == "__main__":
